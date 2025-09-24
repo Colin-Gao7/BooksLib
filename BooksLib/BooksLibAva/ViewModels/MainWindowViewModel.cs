7339d38f43308704cb711bd5b8565d736df1458b
Colin.Gao <gsixin@live.com>
2025-09-24 15:54:19 +0800

    Initialize sln with some projects: host, wpf and avalonia.

---
+++ BooksLib/BooksLibAva/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BooksLibAva.ViewModels
+{
+    public partial class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting { get; } = "Welcome to Avalonia!";
+    }
+}
